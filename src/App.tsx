4533e8620d8b572f64826fa05696fe433f299d2a
Nathanael Glass <nathanael9762@gmail.com>
2025-01-09 21:58:29 +0000

    template refactor

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import MarketingPage from './marketing-page/MarketingPage';
+
+function App() {
+  return (
+    <MarketingPage/>
+  );
+}
+
+export default App;
